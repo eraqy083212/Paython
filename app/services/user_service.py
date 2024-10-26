c5d4c70a95a7f6ec54faf24da25a151ce4ccf1f5
eraqy083212 <ahmed_mostafa_2006@hotmail.com>
2024-10-26 16:07:37 +0300

    Update user_service.py

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,7 +2,7 @@ class UserService:
     def __init__(self):
         self.users = [
             {"id": 1, "name": "Ahmed Eraqy"},
-            {"id": 2, "name": "Jane Doe"}
+            {"id": 2, "name": "Omar"}
         ]
 
     def get_users(self):
